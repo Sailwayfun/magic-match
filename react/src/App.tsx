69a0dfb1b707634aaa58c803c9124fb7df319630
Sailwayfun <liaoleon000513@gmail.com>
2024-02-23 20:54:02 +0800

    styles: button

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <>
+    <div className="mx-10 my-auto max-w-[860px]">
       <h1>Magic match</h1>
-      <button>New Game</button>
-    </>
+      <button className="rounded border-2 border-solid border-white bg-none px-[6px] py-3 text-base font-bold text-white hover:bg-[#c23866]">
+        New Game
+      </button>
+    </div>
   );
 }
 

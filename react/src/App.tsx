629d420b6b92319ed5697038fe6a2c0ab0fe38b5
Sailwayfun <liaoleon000513@gmail.com>
2024-02-23 21:19:44 +0800

    styles: button and body

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="mx-10 my-auto max-w-[860px]">
+    <div className="mx-auto my-10 max-w-[860px]">
       <h1>Magic match</h1>
-      <button className="rounded border-2 border-solid border-white bg-none px-[6px] py-3 text-base font-bold text-white hover:bg-[#c23866]">
+      <button className="rounded border-2 border-solid border-white bg-none px-3 py-[6px] text-base font-bold text-white hover:bg-[#c23866]">
         New Game
       </button>
     </div>
